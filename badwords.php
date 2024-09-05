9308ad7517c430c7629018895aefb6ad19bfa74a
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-09-05 17:24:31 +0200

    [UPD] Esercizio completato

--- badwords.php
+++ badwords.php
@@ -17,7 +17,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./style.css">
-    <title>Document</title>
+    <title>BadWords | Results</title>
 </head>
 <body>
     <div class="results">
